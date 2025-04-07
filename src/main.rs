f8323d38240be91f1be446b8f1b4a95ab8680213
Ayaan Mirza Baig <ayaanmirzabaig85@gmail.com>
2025-04-07 22:39:29 +0530

    Add cd

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,20 @@ fn main() {
         let mut parts = input.trim().split_whitespace();
         let command = parts.next().unwrap();
         let args = parts;
-        let mut child = Command::new(command).args(args).spawn().unwrap();
 
-        child.wait();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
+
+                child.wait();
+            }
+        }
     }
 }
